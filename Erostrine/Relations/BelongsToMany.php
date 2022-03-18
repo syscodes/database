e9237bf7a17700780c5e0360df0f5e90338fb2c4
Alexander Campo <jalexcam@gmail.com>
2022-03-18 14:52:32 -0500

    Added vaiables and construct() method of the class

--- Erostrine/Relations/BelongsToMany.php
+++ Erostrine/Relations/BelongsToMany.php
@@ -22,6 +22,10 @@
 
 namespace Syscodes\Components\Database\Erostrine\Relations;
 
+use Syscodes\Components\Database\Erostrine\Model;
+use Syscodes\Components\Database\Erostrine\Builder;
+use Syscodes\Components\Database\Erostrine\Collection;
+
 /**
  * Relation belongToMany given on the parent model.
  * 
@@ -29,5 +33,120 @@ namespace Syscodes\Components\Database\Erostrine\Relations;
  */
 class BelongsToMany extends Relation
 {
+    /**
+     * The foreign key of the parent model.
+     * 
+     * @var string $foreignKey
+     */
+    protected $foreignKey;
+
+    /**
+     * The associated key of the relation.
+     * 
+     * @var string $ownerKey
+     */
+    protected $ownerKey;
     
+    /**
+     * The pivot table columns to retrieve.
+     * 
+     * @var array $pivotColumns
+     */
+    protected $pivotColumns = [];
+    
+    /**
+     * Any pivot table restrictions.
+     * 
+     * @var array $pivotWheres
+     */
+    protected $pivotWheres = [];
+
+    /**
+     * The "name" of the relationship.
+     * 
+     * @var string $relationName
+     */
+    protected $relationName;
+
+    /**
+     * The intermediate table for the relation.
+     * 
+     * @var string $table
+     */
+    protected $table;
+    
+    /**
+     * The class name of the custom pivot model to use for the relationship.
+     * 
+     * @var string $using
+     */
+    protected $using;
+    
+    /**
+     * Constructor. Create a new has many relationship instance.
+     * 
+     * @param  \Syscodes\Components\Database\Erostrine\Builder  $query
+     * @param  \Syscodes\Components\Database\Erostrine\Model  $parent
+     * @param  string  $table
+     * @param  string  $foreignKey
+     * @param  string  $ownerKey
+     * @param  string|null  $relationName
+     * 
+     * @return void
+     */
+    public function __construct(
+        Builder $query, 
+        Model $parent, 
+        $table, 
+        $foreignKey, 
+        $ownerKey, 
+        $relationName = null
+    ) {
+        $this->table = $table;
+        $this->ownerKey = $ownerKey;
+        $this->foreignKey = $foreignKey;
+        $this->relationName = $relationName;
+        
+        parent::__construct($query, $parent);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResults()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addConstraints(): void
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addEagerConstraints(array $models): void
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function initRelation(array $models, $relation): array
+    {
+        return $models;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function match(array $models, Collection $results, $relation): array
+    {
+        return $models;
+    }
 }
